d7d0652dcc3f92d5b71b1041d539fca3472eb272
sophiastratford <troopersmores82@gmail.com>
2024-06-13 08:54:15 -0700

    Prep3 Guessing Game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        // Console.WriteLine("What is the magic number? ");
+        // string userInput = Console.ReadLine();
+        // int number = int.Parse(userInput);
+
+        // Console.WriteLine("What is your guess? ");
+        // string userGuess = Console.ReadLine();
+        // int guess = int.Parse(userGuess);
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,100);
+
+        int guess = -1;
+
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            if (guess > number)
+            {
+                Console.WriteLine("Lower.");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher.");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
+        }
 }
