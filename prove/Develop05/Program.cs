76e5e6d13ba812354b85a8cb183dd26dadda05f1
sophiastratford <troopersmores82@gmail.com>
2024-07-10 09:01:14 -0700

    Shape Program

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square shape1 = new Square("blue", 3);
+        shapes.Add(shape1);
+
+        Rectangle shape2 = new Rectangle("yellow", 2, 3);
+        shapes.Add(shape2);
+
+        Circle shape3 = new Circle("purple", 4);
+        shapes.Add(shape3);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
