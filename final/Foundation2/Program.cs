05754c69cf3eb266f5b3a1fd530cd236b06ef625
sophiastratford <troopersmores82@gmail.com>
2024-07-24 22:43:09 -0600

    Final tweaks

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -10,9 +10,9 @@ class Program
         Customer customer1 = new("Evelyn Arasim", address1);
         Customer customer2 = new("Alice Rushton", address2);
 
-        Product product1 = new("Boxed Set - Mistborn - Special Edition 2024", 412, 139.99, 1);
-        Product product2 = new("25' Monitor for Desktop", 652, 2199.99, 1);
-        Product product3 = new("Custom Soft Tap Board", 106, 82.99, 3);
+        Product product1 = new("Boxed Set - Mistborn - Special Edition 2024", 482, 139.99, 1);
+        Product product2 = new("25' Monitor for Desktop", 484, 2199.99, 1);
+        Product product3 = new("Custom Soft Tap Board", 486, 82.99, 1);
 
         Order order1 = new(customer1);
         order1.AddProduct(product1);
@@ -25,14 +25,14 @@ class Program
         Console.WriteLine();
         Console.WriteLine(order1.GetShippingLabel());
         Console.WriteLine(order1.GetPackingLabel());
-        Console.WriteLine($"Shipping: ${order1.GetShippingCost():0.00}");
-        Console.WriteLine($"Total Cost: ${order1.GetTotalCost():0.00}");
+        Console.WriteLine($"Shipping: ${order1.GetShippingCost()}");
+        Console.WriteLine($"Total Cost: ${order1.GetTotalCost()}");
         Console.WriteLine();
 
         Console.WriteLine(order2.GetShippingLabel());
         Console.WriteLine(order2.GetPackingLabel());
-        Console.WriteLine($"Shipping: ${order2.GetShippingCost():0.00}");
-        Console.WriteLine($"Total Cost: ${order2.GetTotalCost():0.00}");
+        Console.WriteLine($"Shipping: ${order2.GetShippingCost()}");
+        Console.WriteLine($"Total Cost: ${order2.GetTotalCost()}");
         Console.WriteLine();
     }
 }
